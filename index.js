3323012dc0a364c357486def662908b984227e9a
Ubuntu <ubuntu@ip-172-31-38-22.ec2.internal>
2019-11-13 02:28:20 +0000

    lodwon revised

--- index.js
+++ index.js
@@ -4,9 +4,10 @@
 
 /** identity: Designed to take a value given, and return the value without any changes.
  * 
- *  @param {Array, Number, Boolean, Undefined, String, Object, Null} - Anything - any value.
- * 
- *  @return {Array, Number, Boolean, Undefined, String, Object} Returns the value given unchanged.
+ *  @param {Any Datatype} - Can take in any datatype, an array, number, boolean, undefined,
+ *  string, object, and null.
+ *  
+ * @return {Array, Number, Boolean, Undefined, String, Object} Returns the value given unchanged.
  *
  */
 function identity(value) { 
@@ -15,8 +16,10 @@ function identity(value) {
 module.exports.identity = identity;
 
 /** typeOf:  Designed to check the data type of a particular operand.
- *  @param {String, Sarray, Object, Undefined, Number, Boolean, Null, Function} - Any data type.
- *  @return {String} Returns a string of the data type.
+ *  @param {Any Datatype} Can take on any datatype; an array, string, object, undefined, number, 
+ *  boolean, null, or function.
+ * 
+ * @return {String} Returns a string of the data type.
  */
 function typeOf(value){
     if (Array.isArray(value)){
@@ -32,10 +35,12 @@ module.exports.typeOf = typeOf;
  *  first: Designed to give the value of the first element(0 index) of an array.Returns an empty list
  *  if number is less than zero, and returns the entire array if number is greater than array.length.
  * 
- * @param {Array, Number} Array is used to hold the array of elements, number gives the number of eleemnts wanted.
+ * @param {Array} - Array is used to hold the array of elements.
+ * @param {Number} Number gives the number of elements wanted.
  * 
- * @return {Element of Array] Returns the first element along with all of its properties; if a number is given, it 
- * returns the first items listed in the range. ex: 2 would output the first two numbers.
+ * @return {Element of Array] Returns the first element of the selected elements in an array. Returns the 
+ * element along with all of its properties. When given a number (n) as a parameter, first will return the  
+ * n elements starting at the beginning of the array. 
  */
 function first(array, number){
     let empArr = [];
@@ -62,10 +67,11 @@ module.exports.first = first;
  * last: Designed to give the value of the last element(array.length - 1)) of an array.Returns an empty list
  *  if number is less than zero, and returns the entire array if number is greater than array.length.
  * 
- * @params {Array, Number}  Array is used to hold the array of elements, number gives the number of eleemnts wanted.
+ * @param {Array} Array is used to hold the array of elements.
+ * @param {Number}  Number gives the number of eleemnts wanted.
  * 
- * @return {Element of Array} Returns the last number in array; if a number is given, it returns the last
- * numbers given - ex: the number 2 listed would output two values. 
+ * @return {Element of Array} Returns the last number in array. If a number (n) is given in the parameter, it returns
+ * (n) elements, starting at the end of the array. 
  */
 function last(array, number) {
     let empArr = [];
@@ -95,10 +101,10 @@ module.exports.last = last;
  * returns -1 when an item is not found. If a value has multiple occurance, it returns only the 
  * first occurance of the value. 
  * 
- * @param {Array, Value} Array is used to hold the array of elements, value is the item/value we are 
- * looking for in the array.
+ * @param {Array} Array is used to hold the array of elements.
+ * @param {Value} Value is the item/value we are looking for in the array.
  * 
- * @return {Index} Returns the index of the value given in the argument.
+ * @return {Number} Returns the index of the value given in the argument.
  */
 function indexOf(array, value) {
        for(var i = 0; i < array.length; i++){
@@ -113,8 +119,8 @@ module.exports.indexOf = indexOf;
 /** contains: Designed to determine whether an array contains a certain value using a ternary operator.
  * It should not convert types when comparing and it should return false if the list does not contain an element.
  * 
- * @param {Array, Value} Array is used to hold the array of elements, value is the item/value that
- * we are looking for in the array.
+ * @param {Array} Array is used to hold the array of elements
+ * @param {Value} Value is the item/value that we are looking for in the array.
  * 
  * @return {Boolean} Returns true if value is one of the elements of the array, false if not.
 */
@@ -128,8 +134,8 @@ module.exports.contains = contains;
  * each: Designed to loop over a collection, Array or Object, and applies the 
  * action Function to each value in the collection.
  * 
- * @param {Array or Object} collection: The collection over which to iterate.
- * @param {Function} action: The Function to be applied to each value in the 
+ * @param {Collection(Array/Object) The collection over which to iterate
+ * @param {Function} action: The function to be applied to each value in the 
  * collection
  */
 function each(collection, action) {
@@ -166,12 +172,12 @@ module.exports.unique = unique;
 
 /** filter: Designed to create a new array with all elements that passed the test implemented.
  * 
- * @params: {Array, Function} The array that the function is called upon. Function is the callback
- * used to test each element of the array. It accepts three arguments: element, index, and array,
+ * @param: {Array} The array that the function is calle upon. 
+ * @param: {Function} Function is the callback used to test each element of the array. It accepts three arguments: element, index, and array,
  * and returns true to keep the element, false otherwise.
  * 
- * @return: A new array with the elements that pass the test. If no element passed the test, an
- * emplty array is returned. 
+ * @return: {Array} A new array with the elements that pass the test. If no element passed the test, an
+ * empty array is returned. 
  */
 function filter(array, func) {
     let arr = [];
@@ -189,10 +195,10 @@ module.exports.filter = filter;
  * Arguments: Designed to create a new array from elements that returned a fale value from call
  * function. It is the logical inverse of _.filter.
  * 
- * @params {Array, Function} - holds array used in callback function (2nd parameter). Function
- * accepts 3 arguments: element, index, and array
+ * @param {Array} Holds array used in callback function
+ * @param {Function} - Function accepts 3 arguments: element, index, and array. Used to test each element of the array
  * 
- * @return {Array} Returns a new array of elements for chich calling function returned false.
+ * @return {Array} Returns a new array of elements for which calling function returned false.
  */  
 function reject(array, func){
 
@@ -208,8 +214,9 @@ module.exports.reject = reject;
 /** partition: Designed to call function for each element in array passing it the arguments to filter
  * elements and create an array of arrays where values or split based on whether condition is true or false.
  * 
- * @params (Array, Function) Array holds array used in callback function (2nd parameter) which accepts 3 arguments:
- * element, index, and array.
+ * @param {Array} Array holds array used in callback function
+ * @param {Function) Callback function which accepts 3 arguments element, index, and array. Iterates through array 
+ * to test whether a value is true of false. 
  * 
  * @return {Array} Returns an array that is made up of 2 sub arrays, with false values in one and 
  * true in the other. 
@@ -238,8 +245,9 @@ module.exports.partition = partition;
  * map: Designed to create an array by calling on a specific function on each element present in
  * the parent array.
  * 
- * @params {Collection, Function} Collection is the value passed on to the callback function parameter.
- * Function has 3 arguments: element, index, and collection.
+ * @param {Collection} An array or object passed on to the callback function parameter.
+ * @param {Function} Callback function which as 3 arguments: element index and collection.
+ * 
  * 
  * @return {Array} Returns a new array based off of results from the callback funtion.
  * Arguments:
@@ -256,9 +264,10 @@ module.exports.map = map;
 /** 
  * pluck: Designed to create array by extracting property details from a list. 
  * 
- * @params: {Array of Objects, Property} Loops through details of params to find property values.
+ * @param: {Array of Objects} Values that will be iterated through to find a certain property value.
+ * @param: {Property} The property value we are searching for.
  * 
- * @return: {Array} Returns an array containing a list of values take from the key elements.
+ * @return: {Array} Returns an array containing a list of values taken from the property that we were searching. 
  * 
  * 
  */
@@ -272,13 +281,15 @@ module.exports.pluck = pluck;
  
 /** every: Designed to check collection using a function to see if each element of
  * collection passes a certain test. 
+ *  
+ * @param {Collection} Each element of collection is run through function call once to test values.
+ * @param {Function} Has 3 arguments: element, index, and collection. A function is required for 
+ * every to run since it is used to test the elements in the collection. 
  * 
- * @param {Collection, Function} Collection is run through function call to to test values.
- * 
- * @return {Boolean} Returns true if every element returns a true value, If one element is false
+ * @return {Boolean} Returns true if every element returns a truthy value, If one element is falsy
  * returns false.
  */
- module.exports.pluck = pluck;
+
 function every(collection, func) {
   var value = true;
   if(func === undefined){
@@ -301,10 +312,12 @@ module.exports.every = every;
 /** some: Designed to check collection using a function to see if one element of
  * collection passes a certain test. 
  * 
- * @params {Collection, Function}  Collection is run through function call to to test values.
+ * @param {Collection} Each element of collection is run through function call to test values.
+ * @param {Function} Has 3 arguments: element, index, and collection, the function is required in
+ * order for some to run properly. 
  * 
- * @return {Boolean} Returns true if at least one element returns a true value. Returns false if 
- * collection is empty or none of the elements return a true value. 
+ * @return {Boolean} Returns true if at least one element returns a truthy value. Returns false if 
+ * collection is empty or none of the elements return a truthy value. 
  */
 function some(collection, func){
   var value = false;
@@ -329,9 +342,11 @@ module.exports.some = some;
  * each element of the array. It does not execute the function for array elements without 
  * values and it does not change the original array. 
  * 
- * @params {Array, Function, Seed} Call functions (arguments: previous result, element, index)
- * executes through array, and uses seed on the first iteration as the previous result argument.
- * If no seed is given, it uses teh first element/value of collection.
+ * @param {Array} - Used to iterate over, running function on each element in array.
+ * @param {Function} Call function (arguments: previous result, current result, element, index).
+ * @param {Seed} Used on the first iteration as the previous result argument. If no seed is given,
+ * it uses the first element/value of collection
+ * 
  * 
  * @return {Value/Number} returns the accumulated result from the last call of the callback function.
  */
